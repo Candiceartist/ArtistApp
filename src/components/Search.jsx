059c49cfd9b3cc5643b85464c15382ec081d6721
Candice Jovon Juarez <candice.artist@gmail.com>
2023-03-20 13:16:33 -0400

    input tag

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,16 +2,29 @@ import React from "react";
 import { useState, useEffect } from 'react';
 
 function Search (props) {
-     const [searchData, setsearchData] = useState({searchterm:""});
+    const [searchData, setsearchData] = useState({searchterm:""});
 
-     const handleChange = (event)=> {
+    const handleChange = (event)=> {
         setsearchData({...searchData, [event.target.value]: event.target.value});
-     }
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.artsearch(searchData.searchterm);
+    }
 
 
     return (
         <div>
             <h1>Search</h1>
+            <form>
+                <input 
+                type="text"
+                name="searchterm"
+                value={searchData.searchterm}
+                placeholder="Search Artwork or Artist"  />
+                
+            </form>
         </div>
     )
 }
