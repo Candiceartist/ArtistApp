39b3264b3d900f1d87e19a73884c478a6cfb049d
Candice Jovon Juarez <candice.artist@gmail.com>
2023-03-22 14:26:05 -0400

    updated context

--- src/context/ActivityContext.jsx
+++ src/context/ActivityContext.jsx
@@ -7,10 +7,10 @@ export const Provider = ({ children }) => {
     const [AcitvityData, setActivityData] = useState({
      activities: [
         {
-          name: 'Cait Yomorta',
-          bio: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus placeat nostrum explicabo? Voluptatibus expedita saepe officia optio, commodi totam ratione laudantium ipsum porro molestias, quasi nulla minus vitae laboriosam corrupti Delectus inventore explicabo est odit incidunt rem a recusandae eum pariatur. Aperiam doloremque blanditiis harum voluptate animi fugit beatae asperiores quo, dignissimos sed illum veniam eum accusantium nulla quod voluptatum',
-          scores: 
-        
+          place:"",
+          what:"",
+          pic:"",
+          day:"",
     
         },
 
