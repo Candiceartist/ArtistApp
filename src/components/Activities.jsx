fd1c87188f20827be844435af21e78b674c58313
Candice Jovon Juarez <candice.artist@gmail.com>
2023-03-26 21:10:38 -0400

    divs

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import { useContext,useEffect, useState } from 'react'
 import ActivityContext from '../context/ActivityContext'
@@ -8,31 +9,17 @@ function Activities(today) {
   const {activityData} = useContext(ActivityContext)
     console.log(activityData)
 
-  const [filteredActivites,setFilteredActivities] = useState({activityData})
-  const [daysactive, setDaysactive] = useState()
   
-  const daysactiv = Array.from(
-    new Set(({activityData}).map(({activityData}) => ({activityData}).day))
-  )
-
-  useEffect(() => {
-    setFilteredActivities(
-      ({activityData}).filter(({activityData}) => {
-        return (
-          (!daysactive || daysactive === ({activityData}).day )
-        )
-      })
-    )
-  })
-
    return (
     <div className='dailyActs'>
       
       {activityData.map((key) => {
      return(
       <div>
+      <div className='textBox'>
        <h2>{key.name}</h2>
        <h4>{key.address}</h4>
+       </div>
        </div>
       ) })}
       
@@ -48,5 +35,3 @@ export default Activities
 
 
   
-  
-
